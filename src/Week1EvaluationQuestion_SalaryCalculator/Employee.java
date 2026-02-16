ed9b9fa60a8d2dc42cd7f864cc7714a0772e3020
mikhel-coditas <mikhel.pottella@coditas.com>
2026-02-16 21:36:07 +0530

    the project started in a new file

---
+++ src/Week1EvaluationQuestion_SalaryCalculator/Employee.java
@@ -0,0 +1,9 @@
+package Week1EvaluationQuestion_SalaryCalculator;
+
+public abstract class Employee {
+    double baseSalary;
+    int attendance;
+    int performanceRating;
+    final float PF = 0.12f;
+
+}
